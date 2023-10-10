17f02eea20bc8c315bb1696664bc3cc91db09927
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-10 12:33:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            UseFullTools.sayHi("Jacky");
+        }
+    }
+}
